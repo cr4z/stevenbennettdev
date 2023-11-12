98d1a68fd0ab1f62c6d82be62b7a842a6307b5d7
cr4z <stevencr7zz@gmail.com>
2023-11-11 21:44:05 -0600

    Remove console log

--- src/showcase/items/notator_simulation/tools/modules/use_segment_selector.ts
+++ src/showcase/items/notator_simulation/tools/modules/use_segment_selector.ts
@@ -39,7 +39,6 @@ export default function useSegmentSelectorTools(props: {
     setSelectedSegmentID(freshEvent.segments[lastIndex]?.id);
   }
 
-  console.log("asdf");
   const selectedSegmentIndex =
     draftEvent?.segments.findIndex(
       (segment) => segment.id === selectedSegmentID
